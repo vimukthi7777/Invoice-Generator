0a0412d034ff6813c076f48a0c95cd26ae2b845a
Sahan Vimukthi <svimukthi7777@gmail.com>
2026-03-28 13:24:35 +0530

    Frontend Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
+import MenuBar from './components/MenuBar';
+import LandingPage from './pages/LandingPage/LandingPage';
+import Dashboard from './pages/Dashboard';
+import MainPage from './pages/MainPage';
+import PreviewPage from './pages/PreviewPage';
+
 
 const App = () => {
   return (
-    <div className='container'>Application is started</div>
+   <BrowserRouter>
+    <MenuBar/>
+    <Toaster/>
+    <Routes>
+      <Route path="/" element={<LandingPage/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/generate" element={<MainPage/>}/>
+      <Route path="/preview" element={<PreviewPage/>}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div>
+      Landing Page
+    </div>
+  )
+}
+
+export default LandingPage
