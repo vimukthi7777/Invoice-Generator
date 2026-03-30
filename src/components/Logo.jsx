25655369221e4be0186d4a675ee63a479cf9a124
Sahan Vimukthi <svimukthi7777@gmail.com>
2026-03-30 15:06:54 +0530

    Hero section

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {assets} from "../assets/assets.js"
+
+const Logo = () => {
+  return (
+    <img src={assets.logo} alt='logo' height={60} width={60}/>
+  )
+}
+
+export default Logo
